4b261fcbfa6fcb15cc14841c71a28ce7b1c0aade
Fardan Nozami Ajitama <64827736+CahBantul@users.noreply.github.com>
2023-04-11 22:51:25 +0700

    feat(feat): adding dark mode feature

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -21,12 +21,12 @@ const Details = ({position, company, companyLink, time, address, work}) => {
             href={companyLink}
             target="_blank"
             rel="noopener noreferrer"
-            className="text-primary capitalize"
+            className="text-primary capitalize dark:text-primaryDark"
           >
             @{company}
           </a>
         </h3>
-        <span className="capitalize font-medium text-dark/75">
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75">
           {time} | {address}
         </span>
         <p className="font-medium w-full text-justify">{work}</p>
@@ -49,7 +49,7 @@ const Experience = () => {
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
           style={{scaleY: scrollYProgress}}
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light"
         />
         <ul className="flex flex-col w-full items-start justify-between ml-4">
           <Details
--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -9,8 +9,8 @@ import project1 from '../../public/images/projects/crypto-screener-cover-image.j
 
 const FeaturedArticles = ({type, title, summary, image, link, githubLink}) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl">
-      <div className="absolute top-0 -right-3 -z-10 bg-dark rounded-[2.5rem] w-[101%] h-[103%] rounded-br-3xl" />
+    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl dark:bg-dark dark:border-light">
+      <div className="absolute top-0 -right-3 -z-10 bg-dark dark:bg-light rounded-[2.5rem] w-[101%] h-[103%] rounded-br-3xl" />
       <Link
         className="w-1/2 cursor-pointer overflow-hidden rounded-2lg"
         href={link}
@@ -20,15 +20,15 @@ const FeaturedArticles = ({type, title, summary, image, link, githubLink}) => {
       </Link>
 
       <div className="w-1/2 flex flex-col items-start justify-between pl-6">
-        <span className="text-primary font-medium text-xl">{type}</span>
+        <span className="text-primary font-medium text-xl dark:text-primaryDark">{type}</span>
         <Link
           href={link}
           target="_blank"
           className="hover:underline underline-offset-2"
         >
-          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+          <h2 className="my-2 w-full text-left text-3xl font-bold dark:text-light">{title}</h2>
         </Link>
-        <p className="my-2 font-medium text-dark">{summary}</p>
+        <p className="my-2 font-medium text-dark dark:text-light">{summary}</p>
         <div className="mt-2 flex items-center">
           <Link href={githubLink} target="_blank" className="w-10">
             <GithubIcon />
@@ -36,7 +36,7 @@ const FeaturedArticles = ({type, title, summary, image, link, githubLink}) => {
           <Link
             href={link}
             target="_blank"
-            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark"
           >
             Visit Project
           </Link>
@@ -48,8 +48,8 @@ const FeaturedArticles = ({type, title, summary, image, link, githubLink}) => {
 
 const Article = ({title, image, githubLink, type, link}) => {
   return (
-    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
-      <div className="absolute top-0 -right-3 -z-10 bg-dark rounded-[2rem] w-[101%] h-[103%] rounded-br-3xl" />
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light">
+      <div className="absolute top-0 -right-3 -z-10 bg-dark rounded-[2rem] w-[101%] h-[103%] rounded-br-3xl dark:bg-light" />
 
       <Link
         className="w-full cursor-pointer overflow-hidden rounded-2lg"
@@ -60,7 +60,7 @@ const Article = ({title, image, githubLink, type, link}) => {
       </Link>
 
       <div className="w-full flex flex-col items-start justify-between mt-4">
-        <span className="text-primary font-medium text-xl">{type}</span>
+        <span className="text-primary font-medium text-xl dark:text-primaryDark">{type}</span>
         <Link
           href={link}
           target="_blank"
@@ -92,7 +92,7 @@ const articles = () => {
         <title>Nozami | Articles Page</title>
         <meta name="description" content="about nozami" />
       </Head>
-      <main className="w-full mb-16 flex flex-col items-center justify-center">
+      <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
           <AnimatedText text={'Articles'} className="mb-16" />
           <div className="grid grid-cols-12 gap-24 gap-y-32">
