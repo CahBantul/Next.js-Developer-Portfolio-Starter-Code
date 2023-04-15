5bdb44ba1bd84e2ff5d46359522143a92ecb8afd
Fardan Nozami Ajitama <64827736+CahBantul@users.noreply.github.com>
2023-04-15 16:10:29 +0700

    feat(feat): make about page responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base">
-      <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
+      <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6 xl:py-6">
         <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
         <div className='flex items-center lg:py-2'>
           Build with <span className="text-primary dark:text-primaryDark text-2xl px-1">&#9825;</span>{' '}
@@ -14,7 +14,7 @@ const Footer = () => {
             Nozami
           </Link>
         </div>
-        <Link href={'/'}></Link>
+        <Link href={'/'}>Personal Web</Link>
       </Layout>
     </footer>
   );
