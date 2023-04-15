09eddcdd1bdf5a37309e90d8dde9bcf48a5f5666
Fardan Nozami Ajitama <64827736+CahBantul@users.noreply.github.com>
2023-04-15 13:03:57 +0700

    feat(feat): make homepage responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -61,8 +61,8 @@ const Navbar = () => {
 
   const handleClick = () => setIsOpen(!isOpen);
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative">
-      <button className='flex flex-col justify-center items-center hidden lg:flex' onClick={handleClick}>
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8">
+      <button className='flex-col justify-center items-center hidden lg:flex' onClick={handleClick}>
         <span className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
         <span className={`'bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
         <span className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
