bb2df566ce0d43f0382e392a257ec1eeccd61dd5
Fardan Nozami Ajitama <64827736+CahBantul@users.noreply.github.com>
2023-04-16 05:08:29 +0700

    feat(feat): make responsive articles page

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -12,10 +12,12 @@ const FramerImage = motion(Image)
 
 const FeaturedArticles = ({type, title, summary, image, link, githubLink}) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl dark:bg-dark dark:border-light">
-      <div className="absolute top-0 -right-3 -z-10 bg-dark dark:bg-light rounded-[2.5rem] w-[101%] h-[103%] rounded-br-3xl" />
+    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl dark:bg-dark dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
+      <div className="absolute top-0 -right-3 -z-10 bg-dark dark:bg-light rounded-[2.5rem] w-[101%] h-[103%] rounded-br-3xl
+      xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]
+      " />
       <Link
-        className="w-1/2 cursor-pointer overflow-hidden rounded-2lg"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-2lg lg:w-full"
         href={link}
         target="_blank"
       >
@@ -29,16 +31,16 @@ const FeaturedArticles = ({type, title, summary, image, link, githubLink}) => {
         />
       </Link>
 
-      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark">{type}</span>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">{type}</span>
         <Link
           href={link}
           target="_blank"
           className="hover:underline underline-offset-2"
         >
-          <h2 className="my-2 w-full text-left text-3xl font-bold dark:text-light">{title}</h2>
+          <h2 className="my-2 w-full text-left text-3xl font-bold dark:text-light sm:text-sm">{title}</h2>
         </Link>
-        <p className="my-2 font-medium text-dark dark:text-light">{summary}</p>
+        <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">{summary}</p>
         <div className="mt-2 flex items-center">
           <Link href={githubLink} target="_blank" className="w-10">
             <GithubIcon />
@@ -46,7 +48,9 @@ const FeaturedArticles = ({type, title, summary, image, link, githubLink}) => {
           <Link
             href={link}
             target="_blank"
-            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark
+            sm:px-4 sm:text-base
+            "
           >
             Visit Project
           </Link>
@@ -58,8 +62,8 @@ const FeaturedArticles = ({type, title, summary, image, link, githubLink}) => {
 
 const Article = ({title, image, githubLink, type, link}) => {
   return (
-    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light">
-      <div className="absolute top-0 -right-3 -z-10 bg-dark rounded-[2rem] w-[101%] h-[103%] rounded-br-3xl dark:bg-light" />
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4">
+      <div className="absolute top-0 -right-3 -z-10 bg-dark rounded-[2rem] w-[101%] h-[103%] rounded-br-3xl dark:bg-light md:-right-2" />
 
       <Link
         className="w-full cursor-pointer overflow-hidden rounded-2lg"
@@ -111,8 +115,8 @@ const articles = () => {
       </Head>
       <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
-          <AnimatedText text={'Articles'} className="mb-16" />
-          <div className="grid grid-cols-12 gap-24 gap-y-32">
+          <AnimatedText text={'Articles'} className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl" />
+          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
               <FeaturedArticles
                 title={'Crypto Screener Application'}
@@ -125,7 +129,7 @@ local currency."
                 type={'Featured Article'}
               />
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Article
                 title={'Crypto Screener Application'}
                 image={project1}
@@ -134,7 +138,7 @@ local currency."
                 type={'Featured Article'}
               />
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Article
                 title={'Crypto Screener Application'}
                 image={project1}
@@ -155,7 +159,7 @@ local currency."
                 type={'Featured Article'}
               />
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Article
                 title={'Crypto Screener Application'}
                 image={project1}
@@ -164,7 +168,7 @@ local currency."
                 type={'Featured Article'}
               />
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Article
                 title={'Crypto Screener Application'}
                 image={project1}
